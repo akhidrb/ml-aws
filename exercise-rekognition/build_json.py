2ca1551a663bce5056848ba71252d7df6a75842d
ibrahimkhidr <ibrahim.khidr@oliv.finance>
2024-08-25 09:35:28 +0300

    first commit

---
+++ exercise-rekognition/build_json.py
@@ -0,0 +1,21 @@
+import glob
+import boto3
+import json
+
+def run():
+    client = boto3.client('rekognition')
+    combined = []
+    for filename in glob.glob('public/photos/*.jpeg'):
+        with open(filename, 'rb') as fd:
+            response = client.detect_labels(Image={'Bytes': fd.read()})
+            entry = {  "Filename": filename.replace("public/", "") }
+            #####
+            # Replace this code to populate the Labels key with the response from the service
+            #####
+            entry["Labels"] =  response['Labels']
+            combined.append(entry)
+
+    print(json.dumps(combined, indent=2))
+
+if __name__ == '__main__':
+    run()
